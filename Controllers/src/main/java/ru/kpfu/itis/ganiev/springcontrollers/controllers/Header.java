23a47227a1f3211d07eb699c74fb91f8030913b2
rishat19 <57725670+rishat19@users.noreply.github.com>
2021-02-24 03:03:33 +0300

    HW-02

---
+++ Controllers/src/main/java/ru/kpfu/itis/ganiev/springcontrollers/controllers/Header.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.ganiev.springcontrollers.controllers;
+
+public class Header {
+
+    public String name;
+    public String value;
+
+    public Header(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
